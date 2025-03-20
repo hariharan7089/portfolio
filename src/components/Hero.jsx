af0b71dc2bbae1e92d45061abaa0e609671f71d8
hariharan7089 <hariharank007089@gmail.com>
2025-03-20 06:36:32 +0530

    complete portfolio

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,59 +1,48 @@
-import React from 'react'
-import { TypeAnimation } from 'react-type-animation'
-import Hari from "../assets/Hari.JPG"
+import { motion } from "framer-motion";
+import { styles } from "../style";
+import { ComputersCanvas } from "./canvas";
 
-function Hero() {
-    return (
-        <div className='grid grid-cols-1 min-h-[700px] sm:grid-cols-3 bg-black gap-5 max-w-[1200px] mx-auto py-5 '>
-
-            <div className="my-auto mx-auto">
-                <div className="w-[300px] h-auto lg:w-[400]">
-                    <img src={Hari} alt="" className=' rounded-md w-80 '  />
-                </div>
-            </div>
-            <div className="col-span-2 px-5 ml-4 my-auto">
-                <h1 className='text-2xl text-white sm:text-5xl  lg:text-6xl font-extrabold '>
-                    <span className='primary-color'>
-                        I'm a
-                    </span> <br />
-                    <TypeAnimation
-                        sequence={[
-                            "Frontend Dev",
-                            1000,
-                            "Backend Dev",
-                            1000,
-                            "Data analyst",
-                            1000
-                        ]}
-                        wrapper='span'
-                        speed={50}
-                        repeat={Infinity} />
-                </h1>
-                <p className='text-white'>
-                    My Name Is Hari Haran and I have 1+ Year Experience in Web development
-                </p>
-                <div className="">
-                    <div className="mt-4">
-                        <a
-                            href="https://github.com/hariharan7089"
-                            target="_blank"
-                            className="btn-primary border border-gray-400 px-5 py-3 text-white rounded-md transition-all duration-500 ease-in-out hover:bg-gradient-to-r from-orange-500 to-pink-600/90 hover:scale-105"
-                        >
-                            GitHub
-                        </a>|
-                        <a
-                            href="https://www.linkedin.com/in/hariharanfsd/"
-                            target="_blank"
-                            className="btn-outline px-5 py-3 text-white rounded-md border border-gray-400 transition-all duration-500 ease-in-out hover:bg-gray-700 hover:border-gray-600 hover:scale-105"
-                        >
-                            LinkedIn
-                        </a>
-                    </div>
-
-                </div>
-            </div>
+const Hero = () => {
+ 
+  return (
+    <section className="relative w-full h-screen mx-auto ">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
         </div>
-    )
-}
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Hariharan</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          Passionate about building scalable web applications with modern technologies. <br className="sm:block hidden" />
+          </p>
+          
+        </div>
+      </div>
+      <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
---
+++ src/constants/index.js
@@ -0,0 +1,314 @@
+import {
+  backend,
+  carrent,
+  creator,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  jobit,
+  mobile,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  starbucks,
+  tailwind,
+  tesla,
+  threejs,
+  tripguide,
+  typescript,
+  web,
+  h1
+} from "../assets";
+import kotlin from "../assets/tech/kotlin.svg";
+import postman from "../assets/tech/postman.png";
+import sorting from "../assets/sorting.png";
+import social from "../assets/social.png";
+import resume from "../assets/resume.png";
+import parcare from "../assets/parcare.png";
+import education from "../assets/education.png";
+import work from "../assets/work.png";
+import mysql from "../assets/tech/mysql.png";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+ 
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "UI/UX Designer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "MERN Stack Developer",
+    icon: creator,
+  },
+  {
+    title: "Python Full Stack Developer",
+    icon: h1,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "postman",
+    icon: postman,
+  },
+  {
+    name: "kotlin",
+    icon: kotlin,
+  },
+
+  {
+    name: "mysql",
+    icon: mysql,
+  },
+];
+
+const experiences = [
+  {
+    title: "Bachelor of Technology in Computer Science and Engineering",
+    institution: "Achariya College of Engineering Technology",
+    icon: education,
+    iconBg: "#E6DEDD",
+    date: "September 2021 – May 2025",
+    points: [
+      "Graduating with a CGPA of 8.45, demonstrating strong academic performance in software development and engineering principles.",
+      "Completed coursework in Data Structures, Algorithms, Database Management, and Software Engineering.",
+      "Developed multiple real-world projects, including a No-Code/Low-Code Web Application Builder and an Inventory Management System.",
+      "Won first place three times in college project competitions, showcasing innovation and technical expertise.",
+      "Actively participated in hackathons and coding competitions, enhancing problem-solving and teamwork skills."
+    ],
+  },
+  {
+    title: "Full-Stack Web Developer",
+    company_name: "SkillMate Community",
+    icon: work,
+    iconBg: "#E6DEDD",
+    date: "April 2024 - May 2024",
+    points: [
+      "Developed SkillMate using React.js, Tailwind CSS, and Firebase to create a seamless freelancer-client platform.",
+      "Implemented Firebase Authentication for secure user login and registration, ensuring data privacy and access control.",
+      "Designed a fully responsive UI with Tailwind CSS, optimizing user experience across mobile, tablet, and desktop devices.",
+      "Integrated Firebase Firestore for real-time database updates, enabling instant communication between freelancers and clients.",
+      "Managed state efficiently using React hooks and Context API, improving application performance and scalability.",
+      "Optimized Firebase queries to enhance data retrieval speed, improving platform responsiveness by 40%."
+    ],
+  },
+  {
+    title: "Front-End Developer",
+    company_name: "GzofTech Solution",
+    icon: work,
+    iconBg: "#E6DEDD",
+    date: "Dec 2024 - Feb 2025",
+    points: [
+      "Developed a highly interactive UI for Gzoft using React.js, Tailwind CSS, GSAP, and Framer Motion to enhance user engagement.",
+      "Implemented smooth animations and transitions with GSAP and Framer Motion, improving visual aesthetics and responsiveness.",
+      "Optimized application performance by reducing render times and improving state management using React hooks.",
+      "Worked closely with the backend team to integrate APIs efficiently, ensuring real-time data updates and seamless functionality.",
+      "Enhanced website loading speed by 35% by optimizing assets, lazy loading components, and reducing unnecessary re-renders."
+    ],
+  },
+  {
+    title: "Front-End Developer Intern",
+    company_name: "Vault Of Code",
+    icon: work,
+    iconBg: "#E6DEDD",
+    date: "Oct 2023 – Nov 2024",
+    points: [
+      "Worked on real-world projects using HTML, CSS, and JavaScript, gaining hands-on experience in front-end development.",
+      "Developed responsive and user-friendly web interfaces, improving UI/UX accessibility across different devices.",
+      "Collaborated with a team of developers to optimize website structure and improve loading speeds.",
+      "Refactored existing codebase to follow best practices, reducing technical debt and improving maintainability.",
+      "Performed cross-browser testing to ensure consistent performance and layout across various web browsers.",
+      "Implemented accessibility enhancements, making web applications more inclusive for users with disabilities."
+    ],
+  }
+];
+
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Task Manager",
+    description:
+      "Web application which helps users to create and manage daily tasks, add calendar events, store and view  users PDF file",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/IndiraPriyadharshini-63/REACT-CRUD",
+  },
+  {
+    name: "Sorting Visualizer",
+    description:
+      "Web application using React JS to visualize how various sorting algorithms work",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: sorting,
+    source_code_link:
+      "https://github.com/IndiraPriyadharshini-63/SortingVisualizer",
+  },
+  {
+    name: "ResumeCrafter",
+    description:
+      "A comprehensive web application that allows users to create resume.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "redux",
+        color: "green-text-gradient",
+      },
+      {
+        name: "mui",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: resume,
+    source_code_link:
+      "https://github.com/IndiraPriyadharshini-63/Resume-Builder",
+  },
+  {
+    name: "PARCARE",
+    description:
+      "Positive and engaging user experience design. The main goal of app is ease parking and save user's time by collaborating with private parking areas.",
+    tags: [
+      {
+        name: "UI/UX",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "figma",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "inkscape",
+        color: "green-text-gradient",
+      },
+      {
+        name: "coolers",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: parcare,
+    source_code_link: "https://www.behance.net/gallery/207227903/PARCARE",
+  },
+];
+
+export { experiences, projects, services, technologies, testimonials };
